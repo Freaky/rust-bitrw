8f6bfda088bd1d8af7ccbbc3927d78a361d27a74
Thomas Hurst <tom@hur.st>
2018-04-18 02:57:01 +0100

    Add some documentation.

--- src/lib.rs
+++ src/lib.rs
@@ -6,6 +6,10 @@ const MASKS: [u64; 9] = [
     0, 0b1, 0b11, 0b111, 0b1111, 0b11111, 0b111111, 0b1111111, 0b11111111
 ];
 
+/// `The BitReader` struct adds bit-level reading to any io::Reader.
+///
+/// Most readers should probably be wrapped in a `BufReader` to avoid single-byte
+/// reads.
 #[derive(Debug)]
 pub struct BitReader<R> {
     inner: R,
@@ -14,6 +18,7 @@ pub struct BitReader<R> {
 }
 
 impl<R: io::Read> BitReader<R> {
+    /// Create a new `BitReader` around the given reader.
     pub fn new(inner: R) -> Self {
         Self {
             inner,
@@ -22,17 +27,21 @@ impl<R: io::Read> BitReader<R> {
         }
     }
 
-    #[allow(dead_code)]
+    /// Reset the internal state of the BitReader. The next read will load fresh
+    /// data from the current position of the reader and start from the beginning
+    /// of the first byte returned.
     pub fn reset(&mut self) {
         self.buffer[0] = 0;
         self.unused = 0;
     }
 
+    /// Read a single bit from the reader.
     pub fn read_bit(&mut self) -> io::Result<u8> {
         let bit = self.read_bits(1)?;
         Ok(bit as u8)
     }
 
+    /// Read up to 64 bits from the reader.
     pub fn read_bits(&mut self, nbits: u8) -> io::Result<u64> {
         assert!(nbits <= 64);
 
@@ -57,22 +66,27 @@ impl<R: io::Read> BitReader<R> {
         Ok(ret)
     }
 
-    #[allow(dead_code)]
+    /// Get a reference to the reader.
     pub fn get_ref(&self) -> &R {
         &self.inner
     }
 
+    /// Get a mutable reference to the reader.  Be sure to call `reset` if you
+    /// changed the file position or otherwise mutated it.
     pub fn get_mut(&mut self) -> &mut R {
         &mut self.inner
     }
 
-    #[allow(dead_code)]
+    /// Unwrap this `BitReader`, returning the underlying reader and discarding any
+    /// unread buffered bits.
     pub fn into_inner(self) -> R {
         self.inner
     }
 }
 
 impl<R: io::Read + io::Seek> BitReader<R> {
+    /// Seek to the given *bit* position in the file.  Currently only
+    /// `SeekFrom::Start` and `SeekFrom::End` with negative offsets are supported.
     pub fn seek(&mut self, pos: SeekFrom) -> io::Result<u64> {
         match pos {
             SeekFrom::Start(pos) => {
@@ -107,6 +121,10 @@ impl<R: io::Read + io::Seek> BitReader<R> {
     }
 }
 
+/// The `BitWriter` struct adds bit-level writing to any io::Write.
+///
+/// Most writers should probably be wrapped in a `BufWriter` to avoid single-byte
+/// writes.
 #[derive(Debug)]
 pub struct BitWriter<W> {
     inner: W,
@@ -115,6 +133,7 @@ pub struct BitWriter<W> {
 }
 
 impl<W: io::Write> BitWriter<W> {
+    /// Create a new `BitWriter` around the given writer.
     pub fn new(inner: W) -> Self {
         Self {
             inner,
@@ -123,13 +142,14 @@ impl<W: io::Write> BitWriter<W> {
         }
     }
 
-    #[allow(dead_code)]
+    /// Write a single bit to the writer.
     pub fn write_bit(&mut self, bit: u8) -> io::Result<()> {
         assert!(bit <= 1);
         self.write_bits(1, bit as u64)?;
         Ok(())
     }
 
+    /// Write up to 64 bits to the writer.
     pub fn write_bits(&mut self, nbits: u8, value: u64) -> io::Result<usize> {
         assert!(nbits <= 64);
 
@@ -163,29 +183,53 @@ impl<W: io::Write> BitWriter<W> {
         Ok(nbits as usize)
     }
 
+    /// Flush any pending writes to the underlying buffer, padding with zero bits
+    /// up to the nearest byte if necessary, and returning the number of padding
+    /// bits written.  The sum of `write_bits()` + `flush()` or `flush_bits()`
+    /// will be the total number of bits delivered to the writer, and will
+    /// always end on a byte boundary.
+    ///
+    /// This method should **always** be called prior to calling `into_inner` or
+    /// before allowing the `BitWriter` to go out of scope, or buffered bytes may
+    /// be lost.
+    ///
+    /// This also flushes the underlying writer.
     pub fn flush(&mut self) -> io::Result<usize> {
+        let written = self.flush_bits()?;
+        self.inner.flush()?;
+        Ok(written)
+    }
+
+    /// Exactly the same as `flush()`, only it doesn't call `flush()` on the
+    /// wrapped writer.
+    ///
+    /// This may be useful if you're going to call `into_inner()` to get at the
+    /// wrapped writer in order to perform more bytewise writes, and don't care
+    /// if it's all on stable storage just yet.
+    pub fn flush_bits(&mut self) -> io::Result<usize> {
         if self.unused != 8 {
             self.inner.write_all(&[(self.buffer << self.unused) as u8])?;
-            self.inner.flush()?;
             let written = self.unused;
             self.unused = 8;
             Ok(written as usize)
         } else {
-            self.inner.flush()?;
             Ok(0)
         }
     }
 
-    #[allow(dead_code)]
+    /// Get a reference to the writer.
     pub fn get_ref(&self) -> &W {
         &self.inner
     }
 
-    #[allow(dead_code)]
+    /// Get a mutable reference to the writer.  You should `flush()` or at least
+    /// `flush_bits()` prior to making any changes.
     pub fn get_mut(&mut self) -> &mut W {
         &mut self.inner
     }
 
+    /// Unwrap this `BitWriter`, returning the underlying writer and discarding any
+    /// unwritten buffered bits.  You should call `flush()` if this is undesirable.
     pub fn into_inner(self) -> W {
         self.inner
     }
